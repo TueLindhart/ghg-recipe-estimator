7e77f36048bb2fc05649f41e5e3056daf4c67a60
Tue Thomsen <tuethomsen28@gmail.com>
2025-01-30 21:33:24 +0100

    Remove embedding cache

--- food_co2_estimator/data/vector_store/vector_store.py
+++ food_co2_estimator/data/vector_store/vector_store.py
@@ -1,10 +1,7 @@
-from langchain.embeddings import CacheBackedEmbeddings
-from langchain.storage import LocalFileStore
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 from food_co2_estimator.data.vector_store.variables import (
-    EMBEDDING_CACHE_DIR,
     EMBEDDING_MODEL,
     VECTOR_DB_COLLECTION_NAME,
     VECTOR_DB_PERSIST_DIR,
@@ -12,14 +9,15 @@ from food_co2_estimator.data.vector_store.variables import (
 
 
 def get_vector_store() -> Chroma:
-    underlying_embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
-    embedding_store = LocalFileStore(EMBEDDING_CACHE_DIR)
-    cached_embedder = CacheBackedEmbeddings.from_bytes_store(
-        underlying_embeddings, embedding_store, namespace=underlying_embeddings.model
-    )
+    # underlying_embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
+    # embedding_store = LocalFileStore(EMBEDDING_CACHE_DIR)
+    # cached_embedder = CacheBackedEmbeddings.from_bytes_store(
+    #     underlying_embeddings, embedding_store, namespace=underlying_embeddings.model
+    # )
+    embedder = OpenAIEmbeddings(model=EMBEDDING_MODEL)
     return Chroma(
         collection_name=VECTOR_DB_COLLECTION_NAME,
-        embedding_function=cached_embedder,
+        embedding_function=embedder,
         persist_directory=VECTOR_DB_PERSIST_DIR,
         collection_metadata={"hnsw:search_ef": 100},
     )
