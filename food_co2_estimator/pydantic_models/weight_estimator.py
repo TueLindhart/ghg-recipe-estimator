6cd193828dfcac1ce9dcf8259359d48bccebb202
Tue Thomsen <tuethomsen28@gmail.com>
2025-04-17 00:14:00 +0200

    Give more specific type descriptions

--- food_co2_estimator/pydantic_models/weight_estimator.py
+++ food_co2_estimator/pydantic_models/weight_estimator.py
@@ -5,17 +5,18 @@ from pydantic import BaseModel, Field
 
 class WeightEstimate(BaseModel):
     ingredient: str = Field(
-        description="Exact name of the ingredient without any changes."
+        description="Exact name of the ingredient without any changes. Must be of type str.",
     )
     weight_calculation: str = Field(
-        description="Description of how weights are estimated"
+        description="Description of how weights are estimated. Must be of type str.",
     )
     weight_in_kg: Optional[float] = Field(
-        description="Weight provided in kg", default=None
+        description="Weight provided in kg. Must be float. Defaults to None",
+        default=None,
     )
 
 
 class WeightEstimates(BaseModel):
     weight_estimates: List[WeightEstimate] = Field(
-        description="List of 'WeightEstimate' per ingredient."
+        description="List of 'WeightEstimate' per ingredient. Must be of type list[WeightEstimate].",
     )
