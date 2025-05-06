02928087cb0434202583dc41c8c9417c830ff29a
Tue Thomsen <tuethomsen28@gmail.com>
2025-05-06 17:06:39 +0200

    Fix gradient

--- foodprint/src/lib/components/GradientHeading.svelte
+++ foodprint/src/lib/components/GradientHeading.svelte
@@ -17,12 +17,10 @@
   export let className = "";
 </script>
 
-<FlowbiteHeading
-  {tag}
-  class={`font-bold text-transparent bg-clip-text leading-tight
-          bg-gradient-to-r 
-          from-[#f7797d] via-[#fbd786] to-[#5ED4A3]
-          ${className}`}
->
-  {text}
+<FlowbiteHeading {tag} class={`font-bold leading-tight ${className}`}>
+  <span
+    class="inline-block text-transparent bg-clip-text bg-gradient-to-r from-[#f7797d] via-[#fbd786] to-[#5ED4A3]"
+  >
+    {text}
+  </span>
 </FlowbiteHeading>
