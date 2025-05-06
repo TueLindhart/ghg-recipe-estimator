085a7af562fc6c6a43ea7caaef8a22983a30719a
Tue Thomsen <tuethomsen28@gmail.com>
2025-05-06 18:17:20 +0200

    Update cards

--- foodprint/src/lib/components/IngredientCard.svelte
+++ foodprint/src/lib/components/IngredientCard.svelte
@@ -8,15 +8,17 @@
   <h3 class="text-md font-semibold mb-1 text-[#404040]">{ingredient.name}</h3>
   <p class="text-sm mb-1">
     <span class="font-semibold">Udledning:</span>
-    {ingredient.co2_kg} kg CO2e
+    {ingredient.co2_kg != null ? `${ingredient.co2_kg} kg CO2e` : "-"}
   </p>
   <p class="text-sm mb-1">
     <span class="font-semibold">Vægt:</span>
-    {ingredient.weight_kg} kg
+    {ingredient.weight_kg != null ? `${ingredient.weight_kg} kg` : "-"}
   </p>
   <p class="text-sm mb-2">
     <span class="font-semibold">Udledning pr. kg:</span>
-    {ingredient.co2_per_kg} kg CO2e/kg
+    {ingredient.co2_per_kg != null
+      ? `${ingredient.co2_per_kg} kg CO2e/kg`
+      : "-"}
   </p>
   <Button on:click={() => onShowNotes(ingredient)}>Vis Noter</Button>
 </Card>
