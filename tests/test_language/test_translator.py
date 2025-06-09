2b60869056e3e879927e9792608194168b007cd5
Tue Thomsen <tuethomsen28@gmail.com>
2025-06-10 01:02:45 +0200

    Fix new tests

--- tests/test_language/test_translator.py
+++ tests/test_language/test_translator.py
@@ -1,11 +1,18 @@
-import pytest
-from food_co2_estimator.language.translator import _translate_if_not_english, MyTranslator
-from food_co2_estimator.pydantic_models.recipe_extractor import EnrichedIngredient, EnrichedRecipe
 from deep_translator.exceptions import TranslationNotFound
+
+from food_co2_estimator.language.translator import (
+    MyTranslator,
+    _translate_if_not_english,
+)
+from food_co2_estimator.pydantic_models.recipe_extractor import (
+    EnrichedIngredient,
+    EnrichedRecipe,
+)
 
 
 def _create_enriched_recipe():
     return EnrichedRecipe(
+        title="Kartoffelsalat",
         url="http://example.com",
         ingredients=EnrichedIngredient.from_list(["kartofler", "gulerod"]),
         instructions="Bland kartofler og gulerod",
@@ -14,7 +21,9 @@ def _create_enriched_recipe():
 
 
 def test_translate_fallback_on_failure(mocker):
-    mocker.patch.object(MyTranslator, "translate", side_effect=TranslationNotFound)
+    mocker.patch.object(
+        MyTranslator, "translate", side_effect=TranslationNotFound("mocked failure")
+    )
     recipe = _create_enriched_recipe()
     result = _translate_if_not_english(recipe, "da")
     assert isinstance(result, EnrichedRecipe)
